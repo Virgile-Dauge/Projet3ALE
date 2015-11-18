8eec440d6d912482e81ee8e38ac3158976a4a010
userle <userle@localhost.localdomain>
2015-11-18 17:42:38 +0100

    ajout timers et anti-rebonds

--- package/pinController/driver/pinController.c
+++ package/pinController/driver/pinController.c
@@ -3,16 +3,20 @@
 #include <linux/gpio.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
+#include <linux/hrtimer.h>
 
 #define ON 0
 #define OFF 1
 static int irq_number;
 static int etat = 0;
+unsigned long timer_interval_ns = 1e6;
+static struct hrtimer hr_timer;
+static ktime_t ktime_time, ktime_timeout;
 static struct gpio mygpios[] = {
-		{ 1,GPIOF_OUT_INIT_HIGH, "led1"},
- 		{ 2,GPIOF_OUT_INIT_HIGH, "led2"},
-		{ 4,GPIOF_OUT_INIT_HIGH, "led3"},
-		{ 5,GPIOF_IN, "button"}
+	{ 1,GPIOF_OUT_INIT_HIGH, "led1"},
+	{ 2,GPIOF_OUT_INIT_HIGH, "led2"},
+	{ 4,GPIOF_OUT_INIT_HIGH, "led3"},
+	{ 5,GPIOF_IN, "button"}
 };
 static void leds_off(void){
 	gpio_set_value(1,OFF);
@@ -31,8 +35,11 @@ static void set_leds(int value){
 }
 //Fonction a executer dans une tasklet
 void tasklet_button(void){
-	etat = 1 - etat;
-	set_leds(etat);
+	if (ktime_compare(ktime_sub(ktime_get(),ktime_time), ktime_timeout) == 1) {
+		etat = 1 - etat;
+		set_leds(etat);
+		ktime_time = ktime_get();
+	}
 }
 DECLARE_TASKLET(tasklet_button_id, tasklet_button ,0);
 
@@ -40,16 +47,18 @@ DECLARE_TASKLET(tasklet_button_id, tasklet_button ,0);
 //Dois etre légére
 //Ne Dois Pas faire de sleep
 irqreturn_t buttonHandler(int irq, void *data){
- 	tasklet_schedule(&tasklet_button_id);
+	tasklet_schedule(&tasklet_button_id);
 	return IRQ_HANDLED;	
 }
 
 static int __init tst_init(void)
 {
+	ktime_timeout = ktime_set(0,10000);
+	ktime_time = ktime_get();
 	int err = 0, 
-		pause = 1, 
-		somme_pause=0, 
-		temps_total=2000;
+	pause = 1, 
+	somme_pause=0, 
+	temps_total=2000;
 
 	printk(KERN_INFO"Hello world!\n");
 	err = gpio_request_array(mygpios,ARRAY_SIZE(mygpios));
@@ -63,6 +72,15 @@ static void __exit tst_exit(void)
 	gpio_free_array(mygpios, ARRAY_SIZE(mygpios));
 	free_irq(irq_number,NULL);
 }
+/*
+static int __init timer_init(void) {
+	ktime = ktime_set( 0, timer_interval_ns );
+	hrtimer_init( &hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL );
+	hr_timer.function = &timer_callback;
+	hrtimer_start( &hr_timer, ktime, HRTIMER_MODE_REL );
+	return 0;
+}
+*/
 
 module_init(tst_init);
 module_exit(tst_exit);
